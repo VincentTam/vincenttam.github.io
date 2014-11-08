8023b3e533e97a93179fcd376f89ff222b8ff3b7
Vincent Tam <sere@live.hk>
2014-11-09 00:45:50 +0800

    Site updated at 2014-11-08 16:45:50 UTC

--- javascripts/MathJaxLocal.js
+++ javascripts/MathJaxLocal.js
@@ -40,4 +40,4 @@ MathJax.Hub.Config({
   }
 });
 
-MathJax.Ajax.loadComplete("{{ root_url }}/javascripts/MathJaxLocal.js");
+MathJax.Ajax.loadComplete("https://vincenttam.github.io/javascripts/MathJaxLocal.js");
