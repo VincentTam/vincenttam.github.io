VincentTam/vincenttam.github.io

commit 797bb1024e3eda94b0c15831fb4ac24e9371d784

Converted an old post from HTML to Markdown

source/_posts/2014-02-04-a-reason-for-my-vimrepress-installation-failure.markdown
@@ -1,0 +1,48 @@
+---
+layout: post
+title: "A Reason for My VimRepress Installation Failure"
+date: 2014-02-04 14:11:58 +0800
+categories: Vim
+comments: true
+---
+
+In one of my old posts, I mentioned that I had tried using
+VimRepress.[^pp]  However, this plugin kept throwing errors like this.
+
+<pre class="cli"><code class="UBMono">Confirm Delete [6]: First Post Using VimRepress? [yes/NO] :yes
+<span class="vimErr">Error detected while processing :
+Traceback (most recent call last):
+File &quot;&lt;string&gt;&quot;, line 1, in &lt;module&gt;
+File &quot;/home/owner/.vim/bundle/VimRepress/plugin/vimrepress.py&quot;, line 41, in __check
+echoerr(&quot;something wrong: %s&quot; % e)
+File &quot;/home/owner/.vim/bundle/VimRepress/plugin/vimrepress.py&quot;, line 47, in &lt;lambda&gt;
+echoerr = lambda s: vim.command('echoerr &quot;%s&quot;' % s)
+vim.error: Vim(echoerr):something wrong: Vim(let):E461: Illegal variable name: s:user_input</span>
+<span class="vimErrCont">Press ENTER or type command to continue</span>
+</code></pre>
+
+<!-- more -->
+
+I tried browsing `vimrepress.py` starting from lines 41--50, and using
+something like `*`, but I couldn't understand what it was.  As you can
+see from the last paragraph of this post, I finally decided to switch
+to UltraBlog.vim.
+
+After reading the README of [vim-instant-markdown], I can make a
+better *guess* now.  I directly quote Suan's words here.
+
+{% blockquote Suan Yeo https://github.com/suan/vim-instant-markdown README of vim-instant-markdown %}
+Make sure the ruby gems were installed under your default Ruby (i.e. if you're using RVM, use gem install and NOT sudo gem install as that might cause the gems to be installed under a non-RVM Ruby)
+{% endblockquote %}
+
+Despite having a clue, I *won't* switch back to Blogger since
+WordPress.com and UltraBlog.vim work together well on my computer.
+
+Posted via [UltraBlog.vim][end].
+
+---
+[^pp]: Refer to [*My First Post Using UltraBlog.vim*][pp] for details.
+
+[pp]: /blog/2014/01/12/my-first-post-using-ultrablog-vim.html
+[vim-instant-markdown]: https://github.com/suan/vim-instant-markdown
+[end]: http://0x3f.org/blog/ultrablog-as-an-ultimate-vim-blogging-plugin/

source/_posts/2014-02-04-vimrepress-installation-failure.html
@@ -1,40 +1,0 @@
----
-layout: post
-title: A Reason for My VimRepress Installation Failure
-date: 2014-02-04 14:11:58 +0800
-categories:
-- Vim
-tags:
-- Blogger
-- Ruby
-- UltraBlog
-- VimRepress
-- WordPress
-published: true
-comments: true
----
-<p>In <a href="/blog/2014/01/12/my-first-post-using-ultrablog-vim.html"><em>My First Post Using UltraBlog.vim</em></a>, I mentioned that I had tried using VimRepress. However, this plugin kept throwing errors like this.</p>
-
-<pre class="cli"><code class="UBMono">Confirm Delete [6]: First Post Using VimRepress? [yes/NO] :yes
-<span class="vimErr">Error detected while processing :
-Traceback (most recent call last):
-File &quot;&lt;string&gt;&quot;, line 1, in &lt;module&gt;
-File &quot;/home/owner/.vim/bundle/VimRepress/plugin/vimrepress.py&quot;, line 41, in __check
-echoerr(&quot;something wrong: %s&quot; % e)
-File &quot;/home/owner/.vim/bundle/VimRepress/plugin/vimrepress.py&quot;, line 47, in &lt;lambda&gt;
-echoerr = lambda s: vim.command('echoerr &quot;%s&quot;' % s)
-vim.error: Vim(echoerr):something wrong: Vim(let):E461: Illegal variable name: s:user_input</span>
-<span class="vimErrCont">Press ENTER or type command to continue</span>
-</code></pre>
-
-<p>I tried browsing <code>vimrepress.py</code> starting from lines 41–50, and using something like <code>*</code>, but I couldn't understand what it was. As you can see from the last paragraph of this post, I finally decided to switch to UltraBlog.vim.</p>
-
-<p>After reading the README of <a href="https://github.com/suan/vim-instant-markdown">vim-instant-markdown</a>, I can make a better <em>guess</em> now. I directly quote Suan's words here.</p>
-
-<blockquote>
-    Make sure the ruby gems were installed under your default Ruby (i.e. if you're using RVM, use gem install and NOT sudo gem install as that might cause the gems to be installed under a non-RVM Ruby)
-</blockquote>
-
-<p>Despite having a clue, I <em>won't</em> switch back to Blogger since WordPress.com and UltraBlog.vim work together well on my computer.</p>
-
-<p>Posted via <a href="http://0x3f.org/blog/ultrablog-as-an-ultimate-vim-blogging-plugin/">UltraBlog.vim</a>.</p>

commit 59c6cdef533a7c9b5091777ebaabd1b517f398e7

Fixed a broken link in 797bb10

source/_posts/2014-02-04-a-reason-for-my-vimrepress-installation-failure.markdown
@@ -43,6 +43,6 @@ Posted via [UltraBlog.vim][end].
 ---
 [^pp]: Refer to [*My First Post Using UltraBlog.vim*][pp] for details.
 
-[pp]: /blog/2014/01/12/my-first-post-using-ultrablog-vim.html
+[pp]: /blog/2014/01/12/my-first-post-using-ultrablog-vim/
 [vim-instant-markdown]: https://github.com/suan/vim-instant-markdown
 [end]: http://0x3f.org/blog/ultrablog-as-an-ultimate-vim-blogging-plugin/