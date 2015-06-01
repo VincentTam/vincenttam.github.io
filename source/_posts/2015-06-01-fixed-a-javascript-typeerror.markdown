VincentTam/vincenttam.github.io

commit 6a8288a04c79cb5faced50d454ad754e798fe08d

Added a new post on resizable SVG

source/javascripts/svg_resize_bar.js
@@ -1,0 +1,11 @@
+window.onload = function () {
+  bar = document.getElementById("bar");
+  bar.onchange = function() {
+    val = this.value;
+    lbl = document.getElementById("lbl");
+    lbl.innerHTML = val;
+    obj = document.getElementById("obj");
+    obj.height = val;
+    obj.width = val;
+  }
+}

sass/custom/_styles.scss
@@ -67,6 +67,12 @@ span {
   }
 }
 
+// Show the border for resizable SVG
+.boxed {
+  border-style: solid;
+  border-width: 1px;
+}
+
 // Expr
 p, li {
   pre {

source/_includes/custom/after_footer.html
@@ -22,6 +22,7 @@
     }
 
     getScript("{{ root_url }}/javascripts/modernizr-2.0.js",function() {});
+    getScript("{{ root_url }}/javascripts/svg_resize_bar.js",function() {});
     getScript("https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML,https://vincenttam.github.io/javascripts/MathJaxLocal.js",function() {});
 
     // testing fallback copy

source/_posts/2015-04-11-an-image-with-changeable-size.markdown
@@ -1,0 +1,60 @@
+---
+layout: post
+title: "An Image with Changeable Size"
+date: 2015-04-11 17:14:23 +0800
+comments: true
+categories: [SVG, web technologies]
+---
+
+Background
+---
+
+I sometimes upload pictures for illustrating ideas.  You may see
+[*Two Diagrams Illustrating the Isomorphism Extension Theorem*][pp] in
+this blog for example.
+
+Motivation
+---
+
+Enable users to adjust the size of SVG graphics.
+
+Problem
+---
+
+In the linked post, if the zoom level is too *large*, then part of the
+image will be hidden.
+
+<object type="image/svg+xml" class="boxed"
+  data="/downloads/code/svgpan_1.2.2/IET.svg"
+  width="200" height="200">
+  Your browser does not support SVG
+</object>
+
+This is quite *inconvenient* for users who want a *whole* picture.
+
+Goal
+---
+
+To set up a slide bar which controls the size of an SVG image.
+
+<span>Image size:</span>
+<input id="bar" type="range" name="points" min="200" max="800"
+value="200" step="10">
+<span id="lbl">200</span>
+
+<object type="image/svg+xml" class="boxed" id="obj"
+  data="/downloads/code/resizable/IET.svg"
+  width="200" height="200">
+  Your browser does not support SVG
+</object>
+
+<!-- more -->
+
+Example
+---
+
+Click "download" to view the sample web page.
+
+{% include_code A minimum working example resizable/test.html %}
+
+[pp]: /blog/2015/03/28/two-diagrams-illustrating-the-isomorphism-extension-theorem/

source/downloads/code/resizable/IET.svg
@@ -1,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="61.38pt" height="56.98pt" viewBox="0 0 61.38 56.98" version="1.1">
+<defs>
+<g>
+<symbol overflow="visible" id="glyph0-0">
+<path style="stroke:none;" d=""/>
+</symbol>
+<symbol overflow="visible" id="glyph0-1">
+<path style="stroke:none;" d="M 7.234375 -2.484375 C 7.25 -2.53125 7.28125 -2.609375 7.28125 -2.625 C 7.28125 -2.625 7.109375 -2.90625 6.984375 -2.90625 C 6.890625 -2.90625 6.703125 -2.6875 6.671875 -2.625 C 6.03125 -1.140625 5.84375 -0.640625 4.140625 -0.640625 L 2.6875 -0.640625 C 2.546875 -0.640625 2.515625 -0.640625 2.46875 -0.640625 C 2.359375 -0.65625 2.5 -0.5 2.5 -0.578125 C 2.5 -0.609375 2.5 -0.625 2.546875 -0.8125 L 3.1875 -3.375 L 4.046875 -3.375 C 4.890625 -3.375 4.71875 -3.3125 4.71875 -3.078125 C 4.71875 -3 4.71875 -2.890625 4.640625 -2.59375 C 4.625 -2.546875 4.609375 -2.5 4.609375 -2.46875 C 4.609375 -2.421875 4.828125 -2.203125 4.921875 -2.203125 C 5 -2.203125 5.203125 -2.40625 5.25 -2.5625 L 5.8125 -4.890625 C 5.8125 -4.953125 5.59375 -5.171875 5.515625 -5.171875 C 5.4375 -5.171875 5.234375 -4.953125 5.203125 -4.828125 C 5 -4.078125 5 -4 4.078125 -4 L 3.34375 -4 L 3.90625 -6.234375 C 4 -6.59375 3.84375 -6.46875 4.28125 -6.46875 L 5.6875 -6.46875 C 6.890625 -6.46875 7.015625 -6.34375 7.015625 -5.515625 C 7.015625 -5.28125 7.015625 -5.28125 6.984375 -5 C 6.984375 -4.9375 6.96875 -4.859375 6.96875 -4.8125 C 6.96875 -4.765625 7.171875 -4.53125 7.265625 -4.53125 C 7.375 -4.53125 7.5625 -4.75 7.578125 -4.9375 L 7.78125 -6.671875 C 7.8125 -6.9375 7.59375 -7.109375 7.34375 -7.109375 L 2.296875 -7.109375 C 2.109375 -7.109375 1.828125 -6.9375 1.828125 -6.734375 C 1.828125 -6.625 2.09375 -6.46875 2.28125 -6.46875 C 2.65625 -6.46875 2.75 -6.625 2.75 -6.453125 C 2.75 -6.40625 2.75 -6.40625 2.703125 -6.21875 L 1.390625 -0.953125 C 1.28125 -0.5625 1.453125 -0.640625 0.65625 -0.640625 C 0.484375 -0.640625 0.203125 -0.46875 0.203125 -0.28125 C 0.203125 -0.15625 0.46875 0 0.65625 0 L 5.828125 0 C 6.0625 0 6.234375 -0.171875 6.3125 -0.328125 Z M 7.234375 -2.484375 "/>
+</symbol>
+<symbol overflow="visible" id="glyph0-2">
+<path style="stroke:none;" d="M 3.015625 -3.234375 L 3.984375 -3.234375 C 4.734375 -3.234375 4.640625 -3.234375 4.640625 -2.953125 C 4.640625 -2.875 4.640625 -2.765625 4.5625 -2.46875 C 4.546875 -2.421875 4.53125 -2.375 4.53125 -2.359375 C 4.53125 -2.265625 4.78125 -2.078125 4.828125 -2.078125 C 4.9375 -2.078125 5.109375 -2.265625 5.15625 -2.4375 L 5.703125 -4.609375 C 5.734375 -4.71875 5.734375 -4.734375 5.734375 -4.765625 C 5.734375 -4.78125 5.546875 -5.046875 5.4375 -5.046875 C 5.34375 -5.046875 5.15625 -4.828125 5.109375 -4.671875 C 4.90625 -3.890625 4.859375 -3.875 4 -3.875 L 3.3125 -3.875 L 3.90625 -6.234375 C 4 -6.59375 3.84375 -6.46875 4.28125 -6.46875 L 5.59375 -6.46875 C 6.8125 -6.46875 6.875 -6.296875 6.875 -5.53125 C 6.875 -5.296875 6.875 -5.28125 6.84375 -5 C 6.8125 -4.875 6.8125 -4.84375 6.8125 -4.8125 C 6.8125 -4.765625 7.03125 -4.53125 7.125 -4.53125 C 7.234375 -4.53125 7.40625 -4.75 7.421875 -4.9375 L 7.625 -6.671875 C 7.65625 -6.9375 7.4375 -7.109375 7.1875 -7.109375 L 2.296875 -7.109375 C 2.109375 -7.109375 1.828125 -6.9375 1.828125 -6.734375 C 1.828125 -6.625 2.09375 -6.46875 2.28125 -6.46875 C 2.65625 -6.46875 2.75 -6.625 2.75 -6.453125 C 2.75 -6.40625 2.75 -6.40625 2.703125 -6.21875 L 1.390625 -0.953125 C 1.28125 -0.5625 1.453125 -0.640625 0.65625 -0.640625 C 0.484375 -0.640625 0.203125 -0.46875 0.203125 -0.28125 C 0.203125 -0.15625 0.5 0 0.53125 0 C 0.8125 0 1.5625 -0.03125 1.84375 -0.03125 C 2.171875 -0.03125 3 0 3.328125 0 C 3.421875 0 3.703125 -0.15625 3.703125 -0.34375 C 3.703125 -0.421875 3.65625 -0.453125 3.65625 -0.578125 C 3.453125 -0.640625 3.421875 -0.640625 3.203125 -0.640625 C 2.984375 -0.640625 2.9375 -0.640625 2.6875 -0.65625 C 2.390625 -0.6875 2.53125 -0.5625 2.53125 -0.6875 C 2.53125 -0.703125 2.53125 -0.765625 2.578125 -0.921875 L 3.15625 -3.234375 Z M 3.015625 -3.234375 "/>
+</symbol>
+<symbol overflow="visible" id="glyph1-0">
+<path style="stroke:none;" d=""/>
+</symbol>
+<symbol overflow="visible" id="glyph1-1">
+<path style="stroke:none;" d="M 2.390625 -2.53125 L 3.6875 -2.53125 C 3.765625 -2.53125 4.25 -2.71875 4.25 -3 C 4.25 -3.203125 3.875 -3.390625 3.75 -3.390625 L 1.5625 -3.390625 C 1.359375 -3.390625 1 -3.3125 0.640625 -2.984375 C 0.4375 -2.796875 0.125 -2.296875 0.125 -2.25 C 0.125 -2.15625 0.40625 -1.96875 0.4375 -1.96875 C 0.515625 -1.96875 0.640625 -2.03125 0.6875 -2.09375 C 1.046875 -2.578125 1.34375 -2.53125 1.5 -2.53125 L 1.875 -2.53125 L 1.234375 -0.484375 C 1.203125 -0.375 1.203125 -0.375 1.203125 -0.328125 C 1.203125 -0.140625 1.5625 0.078125 1.625 0.078125 C 1.890625 0.078125 2.125 -0.359375 2.140625 -0.453125 L 2.546875 -2.53125 Z M 2.390625 -2.53125 "/>
+</symbol>
+<symbol overflow="visible" id="glyph1-2">
+<path style="stroke:none;" d="M 4.078125 -2.53125 C 4.1875 -2.53125 4.390625 -2.578125 4.46875 -2.640625 C 4.5625 -2.71875 4.640625 -2.953125 4.640625 -3 C 4.640625 -3.203125 4.265625 -3.390625 4.140625 -3.390625 L 2.359375 -3.390625 C 1.25 -3.390625 0.21875 -2.09375 0.21875 -1.265625 C 0.21875 -0.609375 0.890625 0.0625 1.609375 0.0625 C 2.484375 0.0625 3.671875 -0.921875 3.671875 -1.984375 C 3.671875 -2.09375 3.609375 -2.546875 3.625 -2.53125 Z M 1.609375 -0.515625 C 1.234375 -0.515625 1.109375 -0.578125 1.109375 -1.0625 C 1.109375 -1.28125 1.046875 -2.53125 2.25 -2.53125 C 2.59375 -2.53125 2.765625 -2.59375 2.765625 -2.0625 C 2.765625 -1.9375 2.765625 -1.4375 2.421875 -0.96875 C 2.125 -0.59375 1.875 -0.515625 1.609375 -0.515625 Z M 1.609375 -0.515625 "/>
+</symbol>
+</g>
+</defs>
+<g id="surface1">
+<g style="fill:rgb(0%,0%,0%);fill-opacity:1;">
+  <use xlink:href="#glyph0-1" x="6.64" y="15.44"/>
+</g>
+<path style="fill:none;stroke-width:0.398;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0%,0%,0%);stroke-opacity:1;stroke-miterlimit:10;" d="M -25.050781 64.761719 L -17.261719 64.761719 " transform="matrix(1,0,0,-1,72,72)"/>
+<g style="fill:rgb(0%,0%,0%);fill-opacity:1;">
+  <use xlink:href="#glyph0-2" x="46.95" y="15.44"/>
+</g>
+<g style="fill:rgb(0%,0%,0%);fill-opacity:1;">
+  <use xlink:href="#glyph0-2" x="6.71" y="50.34"/>
+</g>
+<path style="fill:none;stroke-width:0.39847;stroke-linecap:butt;stroke-linejoin:round;stroke:rgb(0%,0%,0%);stroke-opacity:1;stroke-dasharray:2.7893,1.59389;stroke-miterlimit:10;" d="M -11.205625 15.536875 L 12.540469 15.536875 " transform="matrix(1,0,0,-1,30.69,28.49)"/>
+<path style="fill:none;stroke-width:0.39847;stroke-linecap:round;stroke-linejoin:round;stroke:rgb(0%,0%,0%);stroke-opacity:1;stroke-miterlimit:10;" d="M 0.000185 -1.861336 C 1.593935 -1.861336 1.593935 0.001945 0.000185 0.001945 " transform="matrix(-1,0,0,1,19.48456,12.95118)"/>
+<path style="fill:none;stroke-width:0.39847;stroke-linecap:round;stroke-linejoin:round;stroke:rgb(0%,0%,0%);stroke-opacity:1;stroke-miterlimit:10;" d="M -2.123731 2.392586 C -1.674513 0.998055 -0.850294 0.240243 0.00126875 -0.001945 C -0.850294 -0.240226 -1.674513 -0.998039 -2.123731 -2.39257 " transform="matrix(1,0,0,-1,43.2292,12.95118)"/>
+<g style="fill:rgb(0%,0%,0%);fill-opacity:1;">
+  <use xlink:href="#glyph1-1" x="28.52" y="10.61"/>
+</g>
+<path style="fill:none;stroke-width:0.39847;stroke-linecap:butt;stroke-linejoin:round;stroke:rgb(0%,0%,0%);stroke-opacity:1;stroke-miterlimit:10;" d="M -20.084531 -11.521719 L -20.084531 9.529063 " transform="matrix(1,0,0,-1,30.69,28.49)"/>
+<path style="fill:none;stroke-width:0.39847;stroke-linecap:butt;stroke-linejoin:round;stroke:rgb(0%,0%,0%);stroke-opacity:1;stroke-miterlimit:10;" d="M -11.611875 -12.01 L 13.071719 9.39625 " transform="matrix(1,0,0,-1,30.69,28.49)"/>
+<path style="fill:none;stroke-width:0.39847;stroke-linecap:round;stroke-linejoin:round;stroke:rgb(0%,0%,0%);stroke-opacity:1;stroke-miterlimit:10;" d="M -0.000904981 -1.860417 C 1.593688 -1.857441 1.593941 0.00226262 -0.000652077 -0.00071385 " transform="matrix(-0.75818,0.65755,0.65755,0.75818,19.0781,40.50097)"/>
+<path style="fill:none;stroke-width:0.39847;stroke-linecap:round;stroke-linejoin:round;stroke:rgb(0%,0%,0%);stroke-opacity:1;stroke-miterlimit:10;" d="M -2.124497 2.392402 C -1.672017 0.995311 -0.848511 0.239888 -0.000903726 -0.000614306 C -0.850441 -0.237587 -1.673884 -0.996948 -2.123998 -2.389216 " transform="matrix(0.75818,-0.65755,-0.65755,-0.75818,43.762,19.09269)"/>
+<g style="fill:rgb(0%,0%,0%);fill-opacity:1;">
+  <use xlink:href="#glyph1-2" x="33.31" y="35.54"/>
+</g>
+</g>
+</svg>

source/downloads/code/resizable/test.html
@@ -1,0 +1,38 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Testing resizable SVG</title>
+<script type="text/javascript" charset="utf-8">
+window.onload = function () {
+  bar = document.getElementById("bar");
+  bar.onchange = function() {
+    val = this.value;
+    lbl = document.getElementById("lbl");
+    lbl.innerHTML = val;
+    obj = document.getElementById("obj");
+    obj.height = val;
+    obj.width = val;
+  }
+}
+</script>
+<!-- Show the border -->
+<style type="text/css" media="screen">
+.boxed {
+  border-style: solid;
+  border-width: 1px;
+}
+</style>
+</head>
+<body>
+<p>
+<span>Image size:</span>
+<input id="bar" type="range" name="points" min="200" max="800"
+value="200" step="10">
+<span id="lbl">200</span>
+</p>
+<object id="obj" class="boxed" type="image/svg+xml" data="IET.svg"
+  width="200" height="200">
+  Your browser does not support SVG.
+</object>
+</body>
+</html>

commit e97f05c764abd1aa9d7339cfc95c4d3beb3b24a6

A new post on a JavaScript TypeError
This error is caused by 6a8288a.

source/_posts/2015-06-01-fixed-a-javascript-typeerror.markdown
@@ -1,0 +1,44 @@
+---
+layout: post
+title: "Fixed a JavaScript TypeError"
+date: 2015-06-01 17:22:45 +0800
+comments: true
+categories: [SVG, web technologies]
+---
+
+Background
+---
+
+I included [an image with changeable size][pp] in a post written about
+two months ago.
+
+### How/Why does this work?
+
+- In brief: View `/javascripts/svg_resize_bar.js`.
+- For details: click the hyperlink above.
+
+Problem
+---
+
+<picture class="fancybox" title="A error received from Firefox
+  console">
+  <source srcset="/images/posts/SVGResizeErr/null_bar558.png"
+    media="(min-width: 558px)"></source>
+  <img alt="FF throws TypeError" src="/images/posts/SVGResizeErr/null_bar270.png" />
+</picture>
+
+When I browsed any pages except that post which contains the
+changeable SVG image, the above `TypeError` was thrown.
+
+<!-- more -->
+
+Solution
+---
+
+From the error message `TypeError: bar is null` and the filename
+`svg_resize_bar.js` on the right, an ordinary user who knows how to
+write if-else statements should be able to solve this in a few
+seconds: surround the part `bar.onchange = function () { ... }` by the
+if-else statement `if (bar != null ) { ... }`.
+
+[pp]: /blog/2015/04/11/an-image-with-changeable-size/

source/javascripts/svg_resize_bar.js
@@ -1,11 +1,13 @@
 window.onload = function () {
   bar = document.getElementById("bar");
-  bar.onchange = function() {
-    val = this.value;
-    lbl = document.getElementById("lbl");
-    lbl.innerHTML = val;
-    obj = document.getElementById("obj");
-    obj.height = val;
-    obj.width = val;
+  if (bar != null) {
+    bar.onchange = function() {
+      val = this.value;
+      lbl = document.getElementById("lbl");
+      lbl.innerHTML = val;
+      obj = document.getElementById("obj");
+      obj.height = val;
+      obj.width = val;
+    }
   }
 }